65e00e8becde34f641d3824cc045cbfc3895ca43
greaterdane <32749786+greaterdane@users.noreply.github.com>
2017-10-18 11:25:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='stagelib',
-      version='0.1',
+      version='0.2',
       description='Python package for data preparation.',
-      url='http://github.com/yoshi788/stagelib',
+      url='http://github.com/greaterdane/stagelib',
       author='Joshua Elvidge',
-      author_email='joshelvidge88@gmail.com',
+      author_email='mongerdechirris97@gmail.com',
       license='MIT',
       packages=['stagelib'],
       zip_safe=False)
